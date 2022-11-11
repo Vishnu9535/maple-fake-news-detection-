a3071dd20c39f9abcf4e3f6ee067d6b03226a1a4
Vishnu9535 <vk9255888@gmail.com>
2022-11-11 11:07:44 -0500

    google search results

--- fake new starting new/news.py
+++ fake new starting new/news.py
@@ -1,25 +1,12 @@
 from multi_rake import Rake
 from GoogleNews import GoogleNews
-# text_input=input('enter the news to search in ')
-text_input="modi is precident of india"
-def find_words(text_input):
-    rake=Rake()
-    keywords=rake.apply(text_input.lower())
-    # print(keywords)
-    return keywords
-keywords=find_words(text_input)
-def get_search_results(keywords):
-    googlenews=GoogleNews()
-    search_words=""
-    for x in keywords:
-        search_words=search_words+x[0]+" "
-    # print(search_words)
-    googlenews.search(search_words)
-    result=googlenews.result()
-    # print(result["link"])
-    return result
-    # print(result[0]['link'])
 
-r = get_search_results(keywords)
-for d in r:
-    print(d["link"])
+
+def get_search_results(keywords):
+    googlenews = GoogleNews()
+    search_words = ""
+    for x in keywords:
+        search_words = search_words+x[0]+" "
+    googlenews.search(search_words)
+    result = googlenews.result()
+    return result
