8e9560ac26e790c6fc2fc6881048fe0cc132f0ba
Shreyas <shreyass.ranganatha@gmail.com>
2022-11-12 18:55:47 +0530

    POST route

--- maple/__init__.py
+++ maple/__init__.py
@@ -1,15 +1,34 @@
-from flask import Flask,request,redirect,jsonify,render_template
+from flask import Flask, request, redirect, jsonify, render_template, jsonify
+import requests as rq
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+
 @app.route("/query/", methods=["POST", "GET"])
 def query():
-    return request.form.get("url", "NO URL :(")
+    url = request.form.get("URL", None)
+
+    if not url:
+        return jsonify({"type": "error"})
+
+    title = BeautifulSoup(rq.get(url).text, "html.parser").find('h1').text
+
+    result = validate(title)
+
+    return jsonify({"URL": url, "valid": result})
+
+    # if request.method == "POST":
+    #     pass
+
+    # else:
+    #     return "GET not supported."
+
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
 
-if __name__ == "__main__":          
+if __name__ == "__main__":
     app.run(debug=True)
